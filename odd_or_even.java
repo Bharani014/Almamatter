43f388bb90acbbfb38565a2a8df73d6c0bda9135
Bharani014 <87220087+Bharani014@users.noreply.github.com>
2021-07-10 11:13:41 +0530

    Add files via upload

---
+++ odd_or_even.java
@@ -0,0 +1,24 @@
+package javaday5;
+
+import java.util.Scanner;
+
+public class odd_or_even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num;
+		System.out.println("Enter an Integer number:");
+		
+		Scanner input = new Scanner(System.in);
+		num = input.nextInt();
+		
+		if ( num % 2 == 0 )
+			
+		System.out.println("Entered number is even");
+		
+		else
+		System.out.println("Entered number is odd");
+	}
+
+}
